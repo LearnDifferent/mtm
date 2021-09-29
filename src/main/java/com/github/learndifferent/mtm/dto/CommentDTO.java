b2f8e4b9aeedbdd61b0be51a06e35b46452e39fa
zhou <imzhouhepeng@outlook.com>
2021-09-29 11:10:07 +0800

    feat: add Comment Data Transfer Object

---
+++ src/main/java/com/github/learndifferent/mtm/dto/CommentDTO.java
@@ -0,0 +1,52 @@
+package com.github.learndifferent.mtm.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Comment DTO
+ *
+ * @author zhou
+ * @date 2021/9/28
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Builder
+public class CommentDTO implements Serializable {
+
+    /**
+     * Comment id
+     */
+    private Integer commentId;
+
+    /**
+     * content
+     */
+    private String comment;
+
+    /**
+     * Web id
+     */
+    private Integer webId;
+
+    /**
+     * Username
+     */
+    private String username;
+
+    /**
+     * Creation time
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date creationTime;
+
+    private static final long serialVersionUID = 1L;
+}
