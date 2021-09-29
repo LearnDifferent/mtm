c5e327697e08bd55061f08340c2043c0f4b08129
zhou <imzhouhepeng@outlook.com>
2021-09-29 11:05:58 +0800

    feat: add comment service interface and service implementation

---
+++ src/main/java/com/github/learndifferent/mtm/service/CommentService.java
@@ -0,0 +1,85 @@
+package com.github.learndifferent.mtm.service;
+
+import com.github.learndifferent.mtm.annotation.validation.comment.AddCommentCheck;
+import com.github.learndifferent.mtm.entity.CommentDO;
+import java.util.List;
+
+/**
+ * Comment Service
+ *
+ * @author zhou
+ * @date 2021/9/28
+ */
+public interface CommentService {
+
+    /**
+     * Get a comment by id
+     *
+     * @param commentId comment id
+     * @return the comment
+     */
+    CommentDO getCommentById(int commentId);
+
+    /**
+     * Gets comment by web id, username and comment
+     *
+     * @param comment  comment
+     * @param webId    web id
+     * @param username username
+     * @return {@link CommentDO} comment data object
+     */
+    CommentDO getCommentByWebIdAndUsernameAndComment(String comment, int webId, String username);
+
+    /**
+     * Get comments by web id
+     *
+     * @param webId web id
+     * @return the comments
+     */
+    List<CommentDO> getCommentsByWebId(int webId);
+
+    /**
+     * Gets comments by username
+     *
+     * @param username username
+     * @return the comments
+     */
+    List<CommentDO> getCommentsByUsername(String username);
+
+    /**
+     * Delete a comment by id
+     *
+     * @param commentId comment id
+     * @return success or failure
+     */
+    boolean deleteCommentById(int commentId);
+
+    /**
+     * Add a comment
+     *
+     * @param comment  comment
+     * @param webId    web id
+     * @param username username
+     * @return success or failure
+     * @throws com.github.learndifferent.mtm.exception.ServiceException {@link AddCommentCheck}
+     *                                                                  annotation will throw an exception if the
+     *                                                                  username is not the current user's name with
+     *                                                                  the result code of {@link com.github.learndifferent.mtm.constant.enums.ResultCode#PERMISSION_DENIED}
+     *                                                                  and throw an exception if the comment existed
+     *                                                                  with {@link com.github.learndifferent.mtm.constant.enums.ResultCode#COMMENT_EXISTS}.
+     *                                                                  If the website does not exist, then the result
+     *                                                                  code will be {@link com.github.learndifferent.mtm.constant.enums.ResultCode#WEBSITE_DATA_NOT_EXISTS}.
+     *                                                                  If the comment is empty or too long, the result
+     *                                                                  code will be {@link com.github.learndifferent.mtm.constant.enums.ResultCode#COMMENT_EMPTY}
+     *                                                                  and {@link com.github.learndifferent.mtm.constant.enums.ResultCode#COMMENT_TOO_LONG}
+     */
+    boolean addComment(String comment, int webId, String username);
+
+    /**
+     * Update a comment
+     *
+     * @param comment comment
+     * @return success or failure
+     */
+    boolean updateComment(CommentDO comment);
+}
---
+++ src/main/java/com/github/learndifferent/mtm/service/impl/CommentServiceImpl.java
@@ -0,0 +1,69 @@
+package com.github.learndifferent.mtm.service.impl;
+
+import com.github.learndifferent.mtm.annotation.common.Comment;
+import com.github.learndifferent.mtm.annotation.common.Username;
+import com.github.learndifferent.mtm.annotation.common.WebId;
+import com.github.learndifferent.mtm.annotation.validation.comment.AddCommentCheck;
+import com.github.learndifferent.mtm.dto.CommentDTO;
+import com.github.learndifferent.mtm.entity.CommentDO;
+import com.github.learndifferent.mtm.mapper.CommentMapper;
+import com.github.learndifferent.mtm.service.CommentService;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhou
+ * @date 2021/9/28
+ */
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentMapper commentMapper;
+
+    @Autowired
+    public CommentServiceImpl(CommentMapper commentMapper) {this.commentMapper = commentMapper;}
+
+    @Override
+    public CommentDO getCommentById(int commentId) {
+        return commentMapper.getCommentById(commentId);
+    }
+
+    @Override
+    public CommentDO getCommentByWebIdAndUsernameAndComment(String comment, int webId, String username) {
+        return commentMapper.getCommentByWebIdAndUsernameAndComment(comment, webId, username);
+    }
+
+    @Override
+    public List<CommentDO> getCommentsByWebId(int webId) {
+        return commentMapper.getCommentsByWebId(webId);
+    }
+
+    @Override
+    public List<CommentDO> getCommentsByUsername(String username) {
+        return commentMapper.getCommentsByUsername(username);
+    }
+
+    @Override
+    public boolean deleteCommentById(int commentId) {
+        return commentMapper.deleteCommentById(commentId);
+    }
+
+    @Override
+    @AddCommentCheck
+    public boolean addComment(@Comment String comment, @WebId int webId, @Username String username) {
+
+        CommentDTO commentDTO = CommentDTO.builder()
+                .comment(comment).webId(webId).username(username)
+                .creationTime(new Date())
+                .build();
+
+        return commentMapper.addComment(commentDTO);
+    }
+
+    @Override
+    public boolean updateComment(CommentDO comment) {
+        return commentMapper.updateComment(comment);
+    }
+}
