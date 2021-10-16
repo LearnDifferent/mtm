7daf0d989963d8ce959e18f86cbe287d5e71ce91
zhou <imzhouhepeng@outlook.com>
2021-10-16 20:05:32 +0800

    refactor: refactor FilterConfig.java

--- src/main/java/com/github/learndifferent/mtm/config/FilterConfig.java
+++ src/main/java/com/github/learndifferent/mtm/config/FilterConfig.java
@@ -1,9 +1,16 @@
 package com.github.learndifferent.mtm.config;
 
-import com.github.learndifferent.mtm.filter.RequestBodyCacheFilter;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 /**
  * 过滤器配置
@@ -24,5 +31,27 @@ public class FilterConfig {
         bean.addUrlPatterns("/log/in");
 
         return bean;
+    }
+}
+/**
+ * Request Body 的过滤器。
+ * <p>配置完成后会加入到 FilterConfig 中，让 Request Body 可以被重复使用</p>
+ *
+ * @author zhou
+ * @date 2021/09/05
+ */
+class RequestBodyCacheFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        ContentCachingRequestWrapper wrapperRequest = new ContentCachingRequestWrapper(request);
+
+        filterChain.doFilter(wrapperRequest, servletResponse);
     }
 }
