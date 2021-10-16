98752a8efd3cb585d5276193e86c113393e6909e
zhou <imzhouhepeng@outlook.com>
2021-10-16 19:52:52 +0800

    feat: add a converter that can map input request parameter to `SearchMode` enum constant

--- src/main/java/com/github/learndifferent/mtm/config/CustomWebConfig.java
+++ src/main/java/com/github/learndifferent/mtm/config/CustomWebConfig.java
@@ -1,13 +1,18 @@
 package com.github.learndifferent.mtm.config;
 
+import cn.dev33.satoken.interceptor.SaRouteInterceptor;
 import com.github.learndifferent.mtm.annotation.general.page.PageInfoMethodArgumentResolver;
+import com.github.learndifferent.mtm.constant.enums.SearchMode;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * 自定义web配置
+ * 自定义 web 配置
  *
  * @author zhou
  * @date 2021/09/05
@@ -18,5 +23,44 @@ public class CustomWebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new PageInfoMethodArgumentResolver());
+    }
+
+    /**
+     * 利用 SaToken，规定需要登录验证的路径。
+     * <p>这里为了方便，所以按照黑名单的模式拦截需要被拦截的路径。
+     * 实际使用的时候，应该使用拦截所有路径，然后放行个别路径的白名单模式。</p>
+     *
+     * @param registry Interceptor Registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaRouteInterceptor())
+                .addPathPatterns("/mypage", "/find/load", "/home/load", "/admin");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new SearchModeConverter());
+    }
+
+}
+
+/**
+ * Convert string to {@link SearchMode}
+ *
+ * @author zhou
+ * @date 2021/10/16
+ */
+class SearchModeConverter implements Converter<String, SearchMode> {
+
+    @Override
+    public SearchMode convert(String source) {
+        try {
+            return SearchMode.valueOf(source.toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            e.printStackTrace();
+            // Returns default mode if exception
+            return SearchMode.WEB;
+        }
     }
 }
