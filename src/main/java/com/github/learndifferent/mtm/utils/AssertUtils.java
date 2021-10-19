0627b4ef6102a1fdb0595aab971517bd15cdd430
zhou <imzhouhepeng@outlook.com>
2021-10-19 21:36:28 +0800

    refactor: refactor AssertUtils.java

--- src/main/java/com/github/learndifferent/mtm/utils/AssertUtils.java
+++ src/main/java/com/github/learndifferent/mtm/utils/AssertUtils.java
@@ -2,7 +2,6 @@ package com.github.learndifferent.mtm.utils;
 
 import com.github.learndifferent.mtm.constant.enums.ResultCode;
 import com.github.learndifferent.mtm.exception.ServiceException;
-import org.springframework.util.Assert;
 
 /**
  * Assertion utility class that can throw {@link ServiceException}
@@ -13,25 +12,19 @@ import org.springframework.util.Assert;
 public class AssertUtils {
 
     public static void notNull(Object object, ResultCode resultCode) {
-        try {
-            Assert.notNull(object, "");
-        } catch (IllegalArgumentException e) {
+        if (object == null) {
             throw new ServiceException(resultCode);
         }
     }
 
     public static void isNull(Object object, ResultCode resultCode) {
-        try {
-            Assert.isNull(object, "");
-        } catch (IllegalArgumentException e) {
+        if (object != null) {
             throw new ServiceException(resultCode);
         }
     }
 
     public static void isTrue(boolean expression, ResultCode resultCode) {
-        try {
-            Assert.isTrue(expression, "");
-        } catch (IllegalArgumentException e) {
+        if (!expression) {
             throw new ServiceException(resultCode);
         }
     }
