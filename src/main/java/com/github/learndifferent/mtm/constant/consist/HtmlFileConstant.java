3eae253f8eec0e5e6d15f03acb7ce0cb84c56bb6
zhou <imzhouhepeng@outlook.com>
2022-03-10 14:29:28 +0800

    feat: make `HtmlFileConstant` private

--- src/main/java/com/github/learndifferent/mtm/constant/consist/HtmlFileConstant.java
+++ src/main/java/com/github/learndifferent/mtm/constant/consist/HtmlFileConstant.java
@@ -1,53 +1,51 @@
 package com.github.learndifferent.mtm.constant.consist;
 
 /**
- * html 文件常量
+ * Constants related to HTML files
  *
  * @author zhou
  * @date 2021/09/12
  */
-public class HtmlFileConstant {
+public final class HtmlFileConstant {
 
     private HtmlFileConstant() {
     }
 
     /**
-     * HTML 文件的开头
+     * HTML file starts with:
      */
-    public static final String FILE_START = "<!DOCTYPE html><html lang=\"en\">" +
-            "<head><meta charset=\"UTF-8\" content=\"text/html\" http-equiv=\"Content-Type\">" +
-            "<title>Websites</title></head><body><dl>";
+    public static final String FILE_START = "<!DOCTYPE html><html lang=\"en\">"
+            + "<head><meta charset=\"UTF-8\" content=\"text/html\" http-equiv=\"Content-Type\">"
+            + "<title>Websites</title></head><body><dl>";
 
     /**
-     * 在 img 之前
+     * Before img tag:
      */
     public static final String BEFORE_IMG =
             "<dt style=\"border-radius: 25px;border: 2px solid grey;padding: 20px;\"><img src=\"";
 
     /**
-     * 在 url 之前
+     * After img tag and before URL:
      */
     public static final String AFTER_IMG_BEFORE_URL = "\" width=\"35px\" height=\"35px\"><a href=\"";
 
     /**
-     * 在 desc 之前
+     * After URL and before Description:
      */
     public static final String AFTER_URL_BEFORE_DESC = "</a><br>";
 
     /**
-     * 在 desc 之后
+     * After Description:
      */
     public static final String AFTER_DESC = "</dt><br>";
 
-
     /**
-     * 在 title 之前
+     * Before Title:
      */
     public static final String BEFORE_TITLE = "\" target=\"_blank\" style=\"font-size: 25px\">";
 
-
     /**
-     * HTML 文件的结尾
+     * HTML file ends with:
      */
     public static final String FILE_END = "</dl></body></html>";
 }
