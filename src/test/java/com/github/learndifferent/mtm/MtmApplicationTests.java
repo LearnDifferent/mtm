126adc905b2eb367fb7582dbce18559e4245e8a6
zhou <imzhouhepeng@outlook.com>
2022-04-13 22:34:27 +0800

    test: add `UserDTOTest`

--- src/test/java/com/github/learndifferent/mtm/MtmApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package com.github.learndifferent.mtm;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MtmApplicationTests {
-}
---
+++ src/test/java/com/github/learndifferent/mtm/dto/UserDTOTest.java
@@ -0,0 +1,20 @@
+package com.github.learndifferent.mtm.dto;
+
+import com.github.learndifferent.mtm.constant.enums.UserRole;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserDTOTest {
+
+    @Test
+    @DisplayName("Password should be encrypted")
+    void passwordShouldBeEncrypted() {
+        String password = "123456789";
+        UserDTO user = UserDTO.ofNewUser("username", password, UserRole.USER);
+        Assertions.assertNotEquals(password, user.getPassword());
+    }
+}
