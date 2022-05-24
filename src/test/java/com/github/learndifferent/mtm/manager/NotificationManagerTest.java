210021ae96429d8e15b92fafacc20f07700edea0
zhou <imzhouhepeng@outlook.com>
2022-05-24 21:05:17 +0800

    test(NotificationManagerTest): add `shouldTurnOffNotifications`

--- src/test/java/com/github/learndifferent/mtm/manager/NotificationManagerTest.java
+++ src/test/java/com/github/learndifferent/mtm/manager/NotificationManagerTest.java
@@ -1,6 +1,9 @@
 package com.github.learndifferent.mtm.manager;
 
+import static com.github.learndifferent.mtm.constant.consist.KeyConstant.MUTE_NOTIFICATIONS;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Assertions;
@@ -35,4 +38,19 @@ class NotificationManagerTest {
         boolean result = notificationManager.checkIfTurnOffNotifications(username);
         Assertions.assertFalse(result);
     }
+
+    @Test
+    @DisplayName("Should turn off notifications")
+    void shouldTurnOffNotifications() {
+        String username = "user1";
+
+        when(redisTemplate.opsForSet()).thenReturn(redisSetOperations);
+        when(notificationManager.checkIfTurnOffNotifications(username)).thenReturn(false);
+
+        notificationManager.turnOnTurnOffNotifications(username);
+
+        verify(redisSetOperations, times(0)).remove(MUTE_NOTIFICATIONS, username);
+        verify(redisSetOperations, times(1)).add(MUTE_NOTIFICATIONS, username);
+    }
+
 }
