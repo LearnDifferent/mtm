d00208273a591a8509be9ac5ec5942609ef76d43
Zhou <zhou.h.p@outlook.com>
2023-03-15 17:48:32 +0800

    feat: add `UserBookmarkRankingByRoleDTO`

---
+++ src/main/java/com/github/learndifferent/mtm/dto/UserBookmarkRankingByRoleDTO.java
@@ -0,0 +1,43 @@
+package com.github.learndifferent.mtm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * The ranking of the number of bookmarks for each role
+ *
+ * @author zhou
+ * @date 2023/3/15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class UserBookmarkRankingByRoleDTO {
+
+    /**
+     * Ranking
+     */
+    private Integer ranking;
+
+    /**
+     * Role of the user
+     */
+    private String role;
+
+    /**
+     * Number of bookmarks
+     */
+    private Integer bookmarkNumber;
+
+    /**
+     * Username of the user
+     */
+    private String userName;
+
+    private static final long serialVersionUID = 1L;
+}
