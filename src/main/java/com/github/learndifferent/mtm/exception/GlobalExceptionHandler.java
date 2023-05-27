4d989dd1a8eba2a084d67756f14b143a62769ba3
Zhou <zhou.h.p@outlook.com>
2023-05-27 17:26:18 +0800

    feat: add `handleMethodArgumentNotValidException()` and use `log.error()` to print stack trace

--- src/main/java/com/github/learndifferent/mtm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/learndifferent/mtm/exception/GlobalExceptionHandler.java
@@ -4,7 +4,14 @@ import cn.dev33.satoken.exception.NotLoginException;
 import com.github.learndifferent.mtm.constant.enums.ResultCode;
 import com.github.learndifferent.mtm.response.ResultCreator;
 import com.github.learndifferent.mtm.response.ResultVO;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,8 +23,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @date 2021/09/05
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResultVO<?> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+
+        log.error("MethodArgumentNotValidException", e);
+
+        // Get error list if there is any field errors
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        // Get all error messages
+        List<String> errorMessages = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return ResultCreator.result(ResultCode.VALIDATION_FAILED, errorMessages);
+    }
 
     /**
      * 处理自定义的服务型异常
@@ -29,7 +54,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     public ResultVO<?> handleServiceException(final ServiceException e) {
 
-        e.printStackTrace();
+        log.error("Service Exception", e);
 
         ResultCode resultCode = e.getResultCode();
         Object data = e.getData();
@@ -40,7 +65,9 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(NotLoginException.class)
     public ResultVO<?> handleNotLoginException(final NotLoginException e) {
-        e.printStackTrace();
+
+        log.error("Not Login Exception", e);
+
         return ResultCreator.result(ResultCode.NOT_LOGIN);
     }
 }
