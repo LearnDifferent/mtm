142b0505ae120f890cefd5e6cef4e914caf13163
Zhou <zhou.h.p@outlook.com>
2023-08-14 10:42:25 +0800

    refactor(RoleServiceImpl): simplify code

--- src/main/java/com/github/learndifferent/mtm/service/impl/RoleServiceImpl.java
+++ src/main/java/com/github/learndifferent/mtm/service/impl/RoleServiceImpl.java
@@ -2,26 +2,22 @@ package com.github.learndifferent.mtm.service.impl;
 
 import cn.dev33.satoken.stp.StpInterface;
 import com.github.learndifferent.mtm.service.UserService;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
- * sa-token 框架用于获取角色权限的实现
+ * <a href='https://sa-token.cc/doc.html#/use/jur-auth'>sa-token settings</a>
  *
  * @author zhou
  * @date 2021/09/05
  */
 @Service
+@RequiredArgsConstructor
 public class RoleServiceImpl implements StpInterface {
 
     private final UserService userService;
-
-    @Autowired
-    public RoleServiceImpl(UserService userService) {
-        this.userService = userService;
-    }
 
     @Override
     public List<String> getPermissionList(Object o, String s) {
@@ -32,8 +28,6 @@ public class RoleServiceImpl implements StpInterface {
     public List<String> getRoleList(Object loginId, String loginKey) {
 
         String role = userService.getRoleByName((String) loginId);
-        ArrayList<String> list = new ArrayList<>();
-        list.add(role);
-        return list;
+        return Collections.singletonList(role);
     }
 }
