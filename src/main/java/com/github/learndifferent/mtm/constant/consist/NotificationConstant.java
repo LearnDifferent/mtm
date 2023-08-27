c57599311bb4effaef1d06ec553cd3224eba6359
Zhou <zhou.h.p@outlook.com>
2023-08-27 18:09:26 +0800

    feat(NotificationConstant): add `NotificationConstant` to define notification-related constants

---
+++ src/main/java/com/github/learndifferent/mtm/constant/consist/NotificationConstant.java
@@ -0,0 +1,16 @@
+package com.github.learndifferent.mtm.constant.consist;
+
+/**
+ * Constants related to notification
+ *
+ * @author zhou
+ * @date 2023/8/26
+ */
+public final class NotificationConstant {
+
+    private NotificationConstant() {}
+
+    public static final String SYSTEM_NOTIFICATION = "notification_type_system";
+
+    public static final String REPLY_NOTIFICATION = "notification_type_reply";
+}
