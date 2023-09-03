dd7c66440b35ab65779abe60f6fc8d9b179eded9
Zhou <zhou.h.p@outlook.com>
2023-09-03 20:27:15 +0800

    feat: add `IdGeneratorConstant`

---
+++ src/main/java/com/github/learndifferent/mtm/constant/consist/IdGeneratorConstant.java
@@ -0,0 +1,15 @@
+package com.github.learndifferent.mtm.constant.consist;
+
+/**
+ * Constants related to ID generator
+ *
+ * @author zhou
+ * @date 2023/9/3
+ */
+public final class IdGeneratorConstant {
+
+    private IdGeneratorConstant() {
+    }
+
+    public static final int STEP = 1000;
+}
