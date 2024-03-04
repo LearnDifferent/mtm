106110df7e79b2b9d942e3944a26b47667f2028c
Zhou <zhou.h.p@outlook.com>
2024-03-04 16:09:54 +0800

    feat(BatchJobService): add `BatchJobService`

---
+++ src/main/java/com/github/learndifferent/mtm/service/impl/BatchJobServiceImpl.java
@@ -0,0 +1,78 @@
+package com.github.learndifferent.mtm.service.impl;
+
+import com.github.learndifferent.mtm.config.batch.BookmarkViewBatchConfig;
+import com.github.learndifferent.mtm.constant.enums.ResultCode;
+import com.github.learndifferent.mtm.exception.ServiceException;
+import com.github.learndifferent.mtm.service.BatchJobService;
+import com.github.learndifferent.mtm.vo.BatchJobVO;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ * Batch Job Service
+ *
+ * @author zhou
+ * @date 2024/3/4
+ */
+@Service
+@Slf4j
+public class BatchJobServiceImpl implements BatchJobService {
+
+    private final JobLauncher jobLauncher;
+    private final Job bookmarkViewBatchJob;
+
+    public BatchJobServiceImpl(JobLauncher jobLauncher,
+                               @Qualifier(BookmarkViewBatchConfig.JOB_NAME) Job bookmarkViewBatchJob) {
+        this.jobLauncher = jobLauncher;
+        this.bookmarkViewBatchJob = bookmarkViewBatchJob;
+    }
+
+    @Override
+    public BatchJobVO updateBookmarkView() {
+
+        JobParameters parameters = getTimestampJobParameters();
+
+        try {
+            JobExecution execution = jobLauncher.run(bookmarkViewBatchJob, parameters);
+            Long jobId = execution.getJobId();
+
+            ExitStatus status = execution.getExitStatus();
+            String exitCode = status.getExitCode();
+            String exitDescription = status.getExitDescription();
+            long instanceId = execution.getJobInstance().getInstanceId();
+            JobParameters jobParameters = execution.getJobParameters();
+            Map<String, JobParameter> jobParametersMap = jobParameters.getParameters();
+
+            return BatchJobVO.builder()
+                    .jobId(jobId)
+                    .jobInstanceId(instanceId)
+                    .jobParameters(jobParametersMap)
+                    .exitCode(exitCode)
+                    .exitDescription(exitDescription)
+                    .build();
+
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+            log.error("Batch Job Error", e);
+            throw new ServiceException(ResultCode.BATCH_JOB_ERROR);
+        }
+    }
+
+    private JobParameters getTimestampJobParameters() {
+        return new JobParametersBuilder()
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters();
+    }
+}
