206695d04a09aca5d4c89947859968600faed9fa
Zhou <zhou.h.p@outlook.com>
2024-04-17 15:37:49 +0800

    feat(SysMenu): add `@JsonInclude(JsonInclude.Include.NON_EMPTY)` annotation to `children` field

--- src/main/java/com/github/learndifferent/mtm/entity/SysMenu.java
+++ src/main/java/com/github/learndifferent/mtm/entity/SysMenu.java
@@ -1,5 +1,6 @@
 package com.github.learndifferent.mtm.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.github.learndifferent.mtm.config.CustomInstantDeserializer;
@@ -65,5 +66,6 @@ public class SysMenu {
     /**
      * Children Menu
      */
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<SysMenu> children;
 }
