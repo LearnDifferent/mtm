82cb34d05997d732bc0eff7a58cb5d700aa397ba
Zhou <zhou.h.p@outlook.com>
2024-04-18 16:15:46 +0800

    feat: add `SysMenuDTO`

---
+++ src/main/java/com/github/learndifferent/mtm/dto/SysMenuDTO.java
@@ -0,0 +1,50 @@
+package com.github.learndifferent.mtm.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * System Menu Data Transfer Object for creating a new menu
+ *
+ * @author zhou
+ * @date 2024/4/17
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SysMenuDTO implements Serializable {
+
+    private String menuName;
+    /**
+     * Parent Menu ID
+     */
+    private Long parentId;
+    /**
+     * Display Order
+     */
+    private Integer displayOrder;
+    private String permissions;
+    /**
+     * Routing Address
+     */
+    private String path;
+    /**
+     * Vue Component
+     */
+    private String component;
+    /**
+     * Vue Component Query (if any)
+     */
+    private String query;
+    private Boolean isExternalLink;
+    private Boolean isCache;
+    private Boolean isHidden;
+    private String materialDesignIcon;
+    private String createdBy;
+    private String updatedBy;
+    private String remark;
+
+    private static final long serialVersionUID = 1L;
+}
