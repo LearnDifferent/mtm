bc1a9f12a2f8deadd2ed295a42abfd81b367c408
Zhou <zhou.h.p@outlook.com>
2024-04-19 16:15:24 +0800

    feat(System Menu): add validation for menu creation and update requests

--- src/main/java/com/github/learndifferent/mtm/query/SysMenuRequest.java
+++ src/main/java/com/github/learndifferent/mtm/query/SysMenuRequest.java
@@ -1,6 +1,11 @@
 package com.github.learndifferent.mtm.query;
 
+import com.github.learndifferent.mtm.validationgroup.OnCreation;
+import com.github.learndifferent.mtm.validationgroup.OnUpdate;
 import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +21,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class SysMenuRequest implements Serializable {
 
+    @Positive(groups = OnUpdate.class, message = "Menu ID must be a positive number")
+    @NotNull(groups = OnUpdate.class, message = "Menu ID must not be null")
     private Long id;
+
+    @NotBlank(groups = OnCreation.class, message = "Menu name must not be blank")
     private String menuName;
     /**
      * Parent Menu ID
