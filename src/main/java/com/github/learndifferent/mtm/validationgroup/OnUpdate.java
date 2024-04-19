1b4ed43170ede874f645124d472bd53ce5f3fd4a
Zhou <zhou.h.p@outlook.com>
2024-04-19 15:52:12 +0800

    feat: add `OnCreation` and `OnUpdate` validation groups

---
+++ src/main/java/com/github/learndifferent/mtm/validationgroup/OnUpdate.java
@@ -0,0 +1,10 @@
+package com.github.learndifferent.mtm.validationgroup;
+
+/**
+ * Validation group for update operations
+ *
+ * @author zhou
+ * @date 2024/4/19
+ */
+public interface OnUpdate {
+}
